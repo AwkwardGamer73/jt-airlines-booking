083cd5ac72c072a97e58b7dd0d6b1040228551fb
AwkwardGamer73 <132657514+AwkwardGamer73@users.noreply.github.com>
2026-05-22 17:07:59 +1200

    Version 1.002
    +Added schedules API - allows for queries to search database for flights based on origin, destination, and flight departure dates

---
+++ app/api/schedules/route.ts
@@ -0,0 +1,60 @@
+import { NextResponse } from "next/server";
+import { Filter, Document } from "mongodb";
+import { connectDB } from "@/lib/mongodb";
+
+export async function GET(req: Request) {
+    try {
+        const { searchParams } = new URL(req.url);
+
+        //Search parameters for finding scheduled flights
+        const orig = searchParams.get("orig");
+        const dest = searchParams.get("dest");
+        const date1 = searchParams.get("date1");
+        const date2 = searchParams.get("date2");
+
+        const db = await connectDB();
+
+        const query: Filter<Document> = {};
+
+        if (orig) query.origin = orig;
+        if (dest) query.dest = dest;
+
+        //Date range filter
+        if (date1 && date2) {
+            const start = new Date(date1);
+            const end = new Date(date2);
+
+            //Interpret search dates in NZ local time
+            //Server expected to be run in Pacific/Auckland timezone
+            start.setHours(0, 0, 0, 0);
+            end.setHours(23, 59, 59, 999);
+
+            query.depDate = {
+                $gte: start,
+                $lte: end,
+            };
+        }
+
+        //Fetch schedules
+        const schedules = await db
+            .collection("schedules")
+            .find(query)
+            .sort({ depDate: 1 })       //Orders search results by departure date (earliest first)
+            .toArray();
+
+        //Computing seats left
+        const flightsPlusSeatsLeft = schedules.map((flight) => ({
+            ...flight,
+            seatsLeft: flight.seats - (flight.bookings?.length || 0),
+        }));
+
+        return NextResponse.json(flightsPlusSeatsLeft);
+    } catch (error) {
+        console.error("Schedules API error:", error);
+
+        return NextResponse.json(
+            { error: "Failed to fetch schedules." },
+            { status: 500 }
+        );
+    }
+}
